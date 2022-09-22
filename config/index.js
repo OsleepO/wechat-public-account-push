0ea969d7f8cc066ef2d284430e2acfa8fafb1df9
OsleepO <77648125+OsleepO@users.noreply.github.com>
2022-09-22 17:48:21 +0200

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oFiqp5vCMNZ-ZN4M5y-G21ra7J0c",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "iazMaP3qOmxj9pYCOlj3gW28A65XvkDY4GtWqYX-Cgc",
+      useTemplateId: "LQMKCZWQSko8H1-SmyBvN1grMEq-F5c--iWKiUQwptY",
       // 所在省份
       province: "马德里",
       // 所在城市
